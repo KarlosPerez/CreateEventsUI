e06de462116ff831786e927300b2740472e3c9f0
Carlos Perez <legomarth@hotmail.com>
2018-08-17 11:51:48 -0500

    Se sube imagen del resultado final

--- app/src/main/java/projects/karlosp3rez/createeventsui/Common/Common.java
+++
@@ -1,7 +0,0 @@
-package projects.karlosp3rez.createeventsui.Common;
-
-import java.util.HashMap;
-
-public class Common {
-    public static HashMap<Boolean,Integer>isTextViewEnabled = new HashMap<>();
-}
